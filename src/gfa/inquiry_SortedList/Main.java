5eb9372caa15e797a63f0246d3613d6dc0bafcec
Joel S <85464337+joel-s99@users.noreply.github.com>
2021-09-21 10:21:29 -0400

    Updated README.txt

--- src/gfa/inquiry_SortedList/Main.java
+++ src/gfa/inquiry_SortedList/Main.java
@@ -2,17 +2,6 @@ package gfa.inquiry_SortedList;
 
 public class Main {
     public static void main(String[] args) {
-        SortedList s = new SortedList();
-
-        s.add("Abba");
-        s.add("a");
-
-        System.out.println(s);
-        s.clear();
-        System.out.println(s);
-
-        s.add("Alex");
-        System.out.println(s);
 
     }
 }
